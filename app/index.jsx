c0077ef30f5b03ce31b7549c82ec1f1cbba732b8
lenmorld <lenmorld@gmail.com>
2019-02-04 22:46:52 -0500

    index

--- app/index.jsx
+++ app/index.jsx
@@ -5,13 +5,9 @@ import UIManager from './UIManager';
 
 class App extends React.Component {
   render() {
-<<<<<<< HEAD
     return (
       <UIManager />
     );
-=======
-    return <div>React: Hello World!</div>;
->>>>>>> c1.1
   }
 }
 
