3cddf4f03e14d9ee7f90ed01454e7e2f8c6ff4ef
lenmorld <lenmorld@gmail.com>
2019-02-05 23:27:14 -0500

    webpack-dev-server

--- README.md
+++ README.md
@@ -1,10 +1,22 @@
 # React workshop
 
 
-## To follow workshop:
+## To dev/ follow workshop:
 
 ```
 $ npm checkout dev
 $ npm install
 $ npm run dev
+```
+
+server runs on 4001
+
+webpack-dev-server for React on 4000
+
+
+## To deploy:
+
+```
+$ npm run build
+$ npm start
 ```
--- app/index.jsx
+++ app/index.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+
 class App extends React.Component {
+  componentDidMount() {
+    fetch('/api/json').then((res) => res.json()).then(res => console.log(res));
+  }
+
   render() {
     return <div>React: Hello World!</div>;
   }
