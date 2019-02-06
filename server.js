f2e327ae627d0e97ab38ae27ac60fc3f309a8547
lenmorld <lenmorld@gmail.com>
2019-02-06 00:05:06 -0500

    formattingh

--- server.js
+++ server.js
@@ -7,14 +7,14 @@ const port = 4000;
 server.use(express.static('public'));
 
 server.get("/", function (req, res) {
-    res.sendFile(__dirname + '/index.html');
+	res.sendFile(__dirname + '/index.html');
 });
 
 // all backend routes go here under /api
 server.get("/api/json", function (req, res) {
-    res.json({name: "Lenny"});
+	res.json({ name: "Lenny" });
 });
 
 server.listen(port, function () { // Callback function
-    console.log("Running server at " + port);
+	console.log("Running server at " + port);
 });
