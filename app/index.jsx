5e4d0a547c33957a45a9ac7b7747bb610d852499
lenmorld <lenmorld@gmail.com>
2019-02-06 00:21:02 -0500

    sdaasd

--- app/index.jsx
+++ app/index.jsx
@@ -1,11 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-<<<<<<< HEAD
 import UIManager from "./UIManager";
 
-=======
->>>>>>> c1.1
 class App extends React.Component {
 	render() {
 		return <UIManager />;
