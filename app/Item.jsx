83d1205550c7f805876c71bcb9401a3512ccb34a
lenmorld <lenmorld@gmail.com>
2019-02-06 00:22:30 -0500

    formatting

--- app/Item.jsx
+++ app/Item.jsx
@@ -1,12 +1,8 @@
-import React from 'react';
+import React from "react";
 
 class Item extends React.Component {
 	render() {
-		return (
-			<div>
-				...item...
-            </div>
-		);
+		return <div>...item...</div>;
 	}
 }
 
--- app/List.jsx
+++ app/List.jsx
@@ -1,6 +1,5 @@
-
-import React from 'react';
-import Item from './Item';
+import React from "react";
+import Item from "./Item";
 
 class List extends React.Component {
 	render() {
