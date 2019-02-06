7b2440f5893aa198e4580cc26c6c22feb095a314
lenmorld <lenmorld@gmail.com>
2019-02-05 23:43:25 -0500

    sdaasd

--- app/index.jsx
+++ app/index.jsx
@@ -4,15 +4,9 @@ import ReactDOM from "react-dom";
 import UIManager from "./UIManager";
 
 class App extends React.Component {
-<<<<<<< HEAD
 	render() {
 		return <UIManager />;
 	}
-=======
-  render() {
-    return <div>React: Hello World!</div>;
-  }
->>>>>>> c1.1
 }
 
 ReactDOM.render(<App />, document.getElementById("app"));
