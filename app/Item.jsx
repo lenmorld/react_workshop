5658945e49988ef819b97cdd23ef5149222053ed
lenmorld <lenmorld@gmail.com>
2019-02-06 23:24:37 -0500

    Item.jsx

--- app/Item.jsx
+++ app/Item.jsx
@@ -2,7 +2,23 @@ import React from "react";
 
 class Item extends React.Component {
 	render() {
-		return <div>...item...</div>;
+		var item = this.props.item;
+		// console.log("Item:", item);
+
+		return (
+			<div className="item">
+				<div className="left">
+					<iframe src={"https://open.spotify.com/embed/track/" + item.id}
+						width="80" height="80" frameBorder="0" allowtransparency="true" allow="encrypted-media">
+					</iframe>
+				</div>
+				<div className="right">
+					<div className="title">{item.title}</div>
+					<div className="artist">{item.artist}</div>
+					<div className="album">{item.album}</div>
+				</div>
+			</div>
+		);
 	}
 }
 
