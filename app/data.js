2bc02991e193b79722e24e881d843b0c4c6af31c
lenmorld <lenmorld@gmail.com>
2019-02-06 21:22:41 -0500

    add data to boilerplate

---
+++ app/data.js
@@ -0,0 +1,24 @@
+var data = {
+	"music": [
+		{
+			"id": "0c4IEciLCDdXEhhKxj4ThA",
+			"artist": "Muse",
+			"title": "Madness",
+			"album": "The 2nd Law"
+		},
+		{
+			"id": "2QAHN4C4M8D8E8eiQvQW6a",
+			"artist": "One Republic",
+			"title": "I Lived",
+			"album": "Native"
+		},
+		{
+			"id": "5VnDkUNyX6u5Sk0yZiP8XB",
+			"artist": "Imagine Dragons",
+			"title": "Thunder",
+			"album": "Evolve"
+		}
+	]
+}
+
+export default data;
