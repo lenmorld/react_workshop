db5dec53399284eaf7ee472cf6506376b0ff9963
lenmorld <lenmorld@gmail.com>
2019-02-08 23:50:40 -0500

    Giphy hello request - componentDidMount ;)

--- app/ApiTest.jsx
+++ app/ApiTest.jsx
@@ -1,6 +1,18 @@
 import React from "react";
+import axios from "axios";
+
+const GIPHY_API_KEY = "MPHyugKqout4DfiKhja9Oy33uLghaigg";
+const SEARCH_QUERY = "dank+memes";
+const RESULTS_LIMIT = 5;
 
 class ApiTest extends React.Component {
+	componentDidMount() {
+		axios.get(`http://api.giphy.com/v1/gifs/search?q=${SEARCH_QUERY}&api_key=${GIPHY_API_KEY}&limit=${RESULTS_LIMIT}`)
+			.then((data) => {
+				console.log(data);
+			});
+	}
+
 	render() {
 		return (
 			<div>
