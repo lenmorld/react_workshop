d8a96f5dc7fe92e0434d64f9ae3b849b5858e6fa
lenmorld <lenmorld@gmail.com>
2019-02-17 20:48:40 -0500

    stateless

--- app/Item.jsx
+++ app/Item.jsx
@@ -1,25 +1,18 @@
 import React from "react";
 
-class Item extends React.Component {
-	render() {
-		var item = this.props.item;
-		// console.log("Item:", item);
-
-		return (
+const Item = (props) => 
+		(
 			<div className="item">
 				<div className="left">
-					<iframe src={"https://open.spotify.com/embed/track/" + item.id}
+					<iframe src={"https://open.spotify.com/embed/track/" + props.item.id}
 						width="80" height="80" frameBorder="0" allowtransparency="true" allow="encrypted-media">
 					</iframe>
 				</div>
 				<div className="right">
-					<div className="title">{item.title}</div>
-					<div className="artist">{item.artist}</div>
-					<div className="album">{item.album}</div>
+					<div className="title">{props.item.title}</div>
+					<div className="artist">{props.item.artist}</div>
+					<div className="album">{props.item.album}</div>
 				</div>
 			</div>
-		);
-	}
-}
-
+		)
 export default Item;
